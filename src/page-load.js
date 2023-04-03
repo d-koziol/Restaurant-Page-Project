3d60d90c791f2e95bb9fa353919abd5778392206
damcio <d.p.koziol@gmail.com>
2023-04-03 23:30:01 +0200

    createNav function added.

--- src/page-load.js
+++ src/page-load.js
@@ -10,6 +10,34 @@ function createHeader() {
 
   return header;
 }
+function createNav() {
+  const nav = document.createElement("nav");
+  nav.classList.add("nav");
+
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("nav-button");
+  homeButton.textContent = "HenHouse";
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("nav-button");
+  menuButton.textContent = "Chickens";
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("nav-button");
+  contactButton.textContent = "Cluck Us";
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+
+  return nav;
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  return main;
+}
 
 function createFooter() {
   const footer = document.createElement("footer");
@@ -26,6 +54,8 @@ function renderWebsite() {
   const content = document.getElementById("content");
 
   content.appendChild(createHeader());
+  content.appendChild(createNav());
+  content.appendChild(createMain());
   content.appendChild(createFooter());
 }
 
