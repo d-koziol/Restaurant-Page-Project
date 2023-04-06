de7c0ccd7a1dda413528d048161410a4c4cb5f43
damcio <d.p.koziol@gmail.com>
2023-04-06 22:35:01 +0200

    createMenu funtion modified.

--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,27 @@ function createMenu() {
   menu.classList.add("menu");
 }
 
+function createDish(name, description) {
+  const dish = document.createElement("div");
+  dish.classList.add = "dish-card";
+
+  const dishName = document.createElement("h2");
+  dishName.textContent = name;
+
+  const dishDescription = document.createElement("p");
+  dishDescription.textContent = description;
+
+  const dishPic = document.createElement("img");
+  dishPic.src = `images/dish/${name.toLowerCase()}.png`;
+  dishPic.alt = `Picture of ${name}`;
+
+  createMenu.appendChild(dishPic);
+  createMenu.appendChild(dishName);
+  createMenu.appendChild(dishDescription);
+
+  return createDish;
+}
+
 function menuLoad() {
   const main = document.getElementById("main");
   main.textContent = "";
